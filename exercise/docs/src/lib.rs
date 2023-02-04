8784190ea94cd745aa31b85acfcb1c0e47e046dd
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-02-04 16:06:26 +0300

    completed the docs exercise

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -2,6 +2,13 @@
 // paragraph from https://en.wikipedia.org/wiki/Pumpkin and pasting it as our module-level
 // documentation. Hint: Use inner-documentation comments.
 //
+//! A pumpkin is a vernacular term for mature winter squash of species and varieties in
+//! the genus Cucurbita that has culinary and cultural significance but no agreed upon botanical or scientific meaning.
+//! The term pumpkin is sometimes used interchangeably with "squash" or "winter squash",
+//! and is commonly used for cultivars of Cucurbita argyrosperma, Cucurbita ficifolia, Cucurbita maxima, Cucurbita moschata, and Cucurbita pepo.
+//! 
+//! ![pumpkin image](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+
 // Once you've got the documentation here, run `cargo doc --no-deps --open` and take a look!
 
 // 2. What about an image!? Add an image of a pumpkin to the end of the module-level documentation.
@@ -15,21 +22,31 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/// Big orange thing
+//
+/// # Recipes
+//
+/// coming soon...
 pub struct Pumpkin {
+    /// `roundness` is a percentage
     pub roundness: f32,
+    /// `orangeness` is a number from 8 to 27
     pub orangeness: i32,
 }
 
 // 4. Document the "smash" method. Explain that if you smash the pumpkin, it will be gone. Then it
 // can't be used for pie. :'-(
 
+
 impl Pumpkin {
+    /// If you smash the pumpkin, it will be gone. Then it can't be used for pie. :'-(
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// `BURNT_ORANGE` is for the `orangeness` field in the [Pumpkin] struct.
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
